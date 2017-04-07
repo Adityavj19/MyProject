35e3be653247ccfa1a03c4eede25b47d61ef3062
Adityavj19 <adityavijawargiya17@gmail.com>
2017-04-06 19:19:13 -0500

    My 10th commit

---
+++ python10.py
@@ -0,0 +1,15 @@
+cat = "\t I m tabbed in."
+dog = "I m split \n on a line."
+fish = "I  m a fish"
+lion = """
+I'll do a list:
+\t* cat
+\t* dog
+\t* fish
+\t* lion
+"""
+
+print cat
+print dog
+print fish
+print lion
