429afca2ad0ce026f6a7354db9b3bcf02c6b4e58
Adityavj19 <adityavijawargiya17@gmail.com>
2017-04-06 23:24:48 -0500

    my 13th commit

---
+++ python13.py
@@ -0,0 +1,6 @@
+from sys import argv
+
+script, first, second, third = argv
+
+
+print "All together, your script is called %r, your first variable is %r, your second is %r, your third is %r" % (script, first, second, third)
