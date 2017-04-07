c57ffb3aab7ec0a1360c4cb17ca3c248148dcaaa
Adityavj19 <adityavijawargiya17@gmail.com>
2017-04-06 19:19:45 -0500

    My 11th commit

---
+++ python11.py
@@ -0,0 +1,8 @@
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you wait?",
+weight = raw_input()
+
+print "So, you are %r year old, You are %r feet tall, You weight is %r pounds" % (age,height,weight)
