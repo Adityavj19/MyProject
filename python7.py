2efe5b8abcc9ccd9deb2c4dcf78c2f8fb90efe82
Adityavj19 <adityavijawargiya17@gmail.com>
2017-04-06 19:17:03 -0500

    My seventh commit

---
+++ python7.py
@@ -0,0 +1,16 @@
+print "Mary had a little lamb."
+print "Its fleece was white as %s." % 'snow'
+print "and everywhere that mary went"
+print "." * 10
+
+end1 = "C"
+end2 = "g"
+end3 = "h"
+end4 = "j"
+end5 = "l"
+end6 = "i"
+end7 = "m"
+end8 = "o"
+
+print end1 + end2 + end3 + end4 + end5,
+print end6 + end7 + end8
