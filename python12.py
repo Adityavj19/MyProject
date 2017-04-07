4345d97dcb54743ea70dbad521502095ff7cd45a
Adityavj19 <adityavijawargiya17@gmail.com>
2017-04-06 19:20:24 -0500

    My 12th commit

---
+++ python12.py
@@ -0,0 +1,5 @@
+age = raw_input("how pld are you?")
+height = raw_input("how tall are you?")
+weight = raw_input("how much do you weight?")
+
+print "So, you are %r year old, You are %r feet tall, You weight is %r pounds" % (age,height,weight)
