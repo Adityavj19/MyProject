5091f150338efef78ea2badf2ba85fc44bfd4691
Adityavj19 <adityavijawargiya17@gmail.com>
2017-04-06 19:18:24 -0500

    My 9th commit

---
+++ python9.py
@@ -0,0 +1,11 @@
+days = "mon,tue,wed,thu,fri,sat,sun"
+months = "\njan\nfeb\nmar\napr\nmay\njun\njul\naug\nsept\noct\nnov\ndec"
+
+print "here are days: ", days
+print "here are months: ", months
+
+print """
+aaaaaaaaaaaaaaaaaaa.
+bbbbbbbbbbbbbbbbbbb.
+ccccccccccccccccccc.
+ddddddddddddddddddd."""
